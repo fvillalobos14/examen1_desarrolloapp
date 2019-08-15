c36bcecb2281ccf7a18a0a28528084984e5c367a
Francis Villalobos <Francis@Franciss-MacBook-Pro.local>
2019-08-14 21:08:41 -0600

    Work so far

---
+++ src/Components/Home.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import Button from '@material-ui/core/Button'
+import Weather from './Weather'
+
+export class Home extends Component {
+
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            welcomeTitle: 'Bienvenido',
+            appTitle: 'Weather App',
+            month: 0,
+            day: 0
+        };
+
+        this.switchTitles = this.switchTitles.bind(this)
+        this.setMay20 = this.setMay20.bind(this)
+        this.setMay21 = this.setMay21.bind(this)
+        this.setMay22 = this.setMay22.bind(this)
+        this.setMay23 = this.setMay23.bind(this)
+    }
+
+    switchTitles()
+    {
+        this.setState({ welcomeTitle: 'Weather App', appTitle: 'Bienvenido' });
+    }
+
+    setMay20(){
+        this.setState({ month: 'May', day: '20'})
+    }
+
+    setMay21(){
+        this.setState({ month: 'May', day: '21'})
+    }
+
+    setMay22(){
+        this.setState({ month: 'May', day: '22'})
+    }
+
+    setMay23(){
+        this.setState({ month: 'May', day: '23'})
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.welcomeTitle}</h1>
+                <h1>{this.state.appTitle}</h1>
+                <Button variant="contained" color="primary" onClick={this.switchTitles}>Intercalar</Button>
+                <hr/>
+                <div>
+                    <Button variant="contained" color="secondary" onClick={this.setMay20}>Weather for May 20</Button>
+                    <Button variant="contained" color="primary" onClick={this.setMay21}>Weather for May 21</Button>
+                </div>
+                <div>
+                    <Button variant="contained" color="secondary" onClick={this.setMay22}>Weather for May 22</Button>
+                    <Button variant="contained" color="primary" onClick={this.setMay23}>Weather for May 23</Button>
+                </div>
+                <hr/>
+                <div>
+                    <Weather weatherMonth={this.state.month} weatherDay={this.state.day}/>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home
---
+++ src/Components/Weather.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import Card from '@material-ui/core/Card'
+import PropTypes from 'prop-types'
+
+export class Weather extends Component {
+
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            weatherItems: [
+                { month: 'May', day: '20', max: '38', min: '18' },
+                { month: 'May', day: '21', max: '30', min: '20' },
+                { month: 'May', day: '22', max: '33', min: '17' },
+                { month: 'May', day: '23', max: '31', min: '19' }
+            ]
+        };
+    }
+
+    //need to work on this part; haven't placed proper variables, but at least binding works
+    render() {
+        return (
+            <div>
+                <Card>
+                    <h2>{this.props.weatherMonth}</h2> 
+                </Card>
+                <br/>
+                <Card>
+                    <h2>{this.props.weatherDay}</h2>
+                </Card>
+            </div>
+        )
+    }
+}
+
+Weather.propTypes = {
+    weatherMonth: PropTypes.string,
+    weatherDay: PropTypes.string
+}
+
+Weather.defaultProps = {
+    weatherMonth: 'May',
+    weatherDay: '20'
+}
+
+export default Weather
